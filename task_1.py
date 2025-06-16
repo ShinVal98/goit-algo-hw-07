a2b1317a1d5508f6af3a164e04f109addb5d8525
ShinVal98 <75795141+ShinVal98@users.noreply.github.com>
2025-06-16 21:03:53 +0200

    Create task_1.py

---
+++ task_1.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        self.root = self._insert(self.root, key)
+
+    def _insert(self, node, key):
+        if node is None:
+            return Node(key)
+        if key < node.key:
+            node.left = self._insert(node.left, key)
+        else:
+            node.right = self._insert(node.right, key)
+        return node
+
+    def find_max(self):
+        return self._find_max(self.root)
+
+    def _find_max(self, node):
+        if node is None:
+            return None
+        while node.right:
+            node = node.right
+        return node.key
