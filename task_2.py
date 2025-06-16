26a79a6f7c107714e25d3aad0885e8bb70b2e3b3
ShinVal98 <75795141+ShinVal98@users.noreply.github.com>
2025-06-16 21:07:14 +0200

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -5,20 +5,27 @@ class Node:
         self.right = None
 
 
-def insert(root, key):
-    if root is None:
-        return Node(key)
-    if key < root.key:
-        root.left = insert(root.left, key)
-    else:
-        root.right = insert(root.right, key)
-    return root
+class BST:
+    def __init__(self):
+        self.root = None
 
+    def insert(self, key):
+        self.root = self._insert_recursive(self.root, key)
 
-def find_min_value(root):
-    if root is None:
-        return None
-    current = root
-    while current.left is not None:
-        current = current.left
-    return current.key
+    def _insert_recursive(self, node, key):
+        if node is None:
+            return Node(key)
+        if key < node.key:
+            node.left = self._insert_recursive(node.left, key)
+        else:
+            node.right = self._insert_recursive(node.right, key)
+        return node
+
+    def find_min(self):
+        if self.root is None:
+            return None
+        current = self.root
+        while current.left:
+            current = current.left
+        return current.key
+
